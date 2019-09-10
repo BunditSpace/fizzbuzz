721feb4fb2149ec275abb52175a37e7d49aa11b8
BunditSpace <spidar21@gmail.com>
2019-09-10 11:34:38 +0700

    fizzbuzz unittest golang

---
+++ fizzbuzz_test.go
@@ -0,0 +1,45 @@
+package fizzbuzz
+
+import (
+	"testing"
+)
+
+func TestInput1ShouldBeDisplay(t *testing.T) {
+	v := FizzBuzz(1)
+
+	if "1" != v {
+		t.Error("fizzbuzz of 1 should be '1' but have ", v)
+	}
+}
+
+func TestInput2ShouldBeDisplay2(t *testing.T) {
+	v := FizzBuzz(2)
+
+	if "2" != v {
+		t.Error("fizzbuzz of 2 should be '2' but have ", v)
+	}
+}
+
+func TestInput3ShouldBeDisplayFizz(t *testing.T) {
+	v := FizzBuzz(3)
+
+	if "Fizz" != v {
+		t.Error("fizzbuzz of 3 should be 'Fizz' but have ", v)
+	}
+}
+
+func TestInput5ShouldBeDisplayBuzz(t *testing.T) {
+	v := FizzBuzz(5)
+
+	if "Buzz" != v {
+		t.Error("fizzbuzz of 5 should be 'Buzz' but have ", v)
+	}
+}
+
+func TestInput15ShouldBeDisplayFizzBuzz(t *testing.T) {
+	v := FizzBuzz(15)
+
+	if "FizzBuzz" != v {
+		t.Error("fizzbuzz of 5 should be 'FizzBuzz' but have ", v)
+	}
+}
